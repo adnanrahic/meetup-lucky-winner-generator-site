f8830f9e251b67569bc8f81b3f6cd7b2a2533958
adnanrahic <ado.raha198@gmail.com>
2018-04-27 16:35:19 +0200

    fresh start

---
+++ backend/handler.js
@@ -0,0 +1,60 @@
+'use strict';
+const meetupLuckyWinnerGenerator = require('meetup-lucky-winner-generator');
+
+module.exports.generateOne = (event, context) => {
+  const options = {
+    meetup: event.pathParameters.meetup,
+    eventId: event.pathParameters.eventId,
+    apiKey: process.env.API_KEY
+  };
+  return meetupLuckyWinnerGenerator
+    .generateOneLuckyWinner(options)
+    .then(luckyWinner => ({
+      statusCode: 200,
+      body: JSON.stringify(luckyWinner)
+    }))
+    .catch(err => ({
+      statusCode: err.statusCode || 500,
+      headers: { 'Content-Type': 'application/json' },
+      body: { message: err.message }
+    }));
+};
+
+module.exports.generateThree = (event, context) => {
+  const options = {
+    meetup: event.pathParameters.meetup,
+    eventId: event.pathParameters.eventId,
+    apiKey: process.env.API_KEY
+  };
+  return meetupLuckyWinnerGenerator
+    .generateThreeLuckyWinners(options)
+    .then(luckyWinners => ({
+      statusCode: 200,
+      body: JSON.stringify(luckyWinners)
+    }))
+    .catch(err => ({
+      statusCode: err.statusCode || 500,
+      headers: { 'Content-Type': 'application/json' },
+      body: { message: err.message }
+    }));
+};
+
+// module.exports.generateCustom = (event, context) => {
+//   const options = {
+//     meetup: event.pathParameters.meetup,
+//     eventId: event.pathParameters.eventId,
+//     numberOfWinners: event.pathParameters.numberOfWinners,
+//     apiKey: process.env.API_KEY
+//   };
+//   return meetupLuckyWinnerGenerator
+//     .generateLuckyWinnersByNumber(options)
+//     .then(luckyWinners => ({
+//       statusCode: 200,
+//       body: JSON.stringify(luckyWinners)
+//     }))
+//     .catch(err => ({
+//       statusCode: err.statusCode || 500,
+//       headers: { 'Content-Type': 'application/json' },
+//       body: { message: err.message }
+//     }));
+// };
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Meetup | Lucky Winner Generator
+
+A simple website to generate lucky winners at your Meetup event!
+
+Add your Meetup name and event id, hit the `generate` button and give away awesome freebies.
